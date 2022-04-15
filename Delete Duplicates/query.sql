082c2e445dd83bac6127755171fcafe7369886b9
Nazarudin Ahmad <43535279+nnzrr@users.noreply.github.com>
2022-04-16 05:44:35 +0700

    Update query.sql

--- Delete Duplicates/query.sql
+++ Delete Duplicates/query.sql
@@ -1,29 +1,33 @@
 --showing duplicate rows
 WITH test AS (SELECT *, ROW_NUMBER() OVER( PARTITION BY parcelid, 
-						 				                                    propertyaddress,
-						 				                                    saleprice, 
-						 			                                    	saledate, 
-						 				                                    legalreference,
-										                                    taxdistrict, 
-										                                    owneraddress, 
-						 				                                    ownername ORDER BY uniqueid ) AS row_num
-										                                      FROM public.nashville_housing)
+							propertyaddress,
+							saleprice, 
+						 	saledate, 
+						 	legalreference,
+							taxdistrict, 
+							owneraddress, 
+						 	ownername ORDER BY uniqueid ) AS row_num
+							 FROM public.nashville_housing)
  
- SELECT * FROM test WHERE row_num > 1
+SELECT * FROM test WHERE row_num > 1
+--the duplicate with row_num > 1 will show
  
- DELETE FROM public.nashville_housing
-WHERE uniqueid IN
-(SELECT uniqueid FROM (SELECT uniqueid, ROW_NUMBER() OVER( PARTITION BY 	parcelid, 
-						 				propertyaddress,
-						 				saleprice, 
-						 				saledate, 
-						 				legalreference,
-										taxdistrict, 
-										owneraddress, 
-						 				ownername ORDER BY uniqueid ) AS row_num
-										FROM public.nashville_housing) n 
- 										WHERE n.row_num > 1)
- 
- DELETE 103
 
-Query returned successfully in 286 msec.
+-- Delete Duplicate rows
+DELETE FROM public.nashville_housing
+	WHERE uniqueid IN
+		(SELECT uniqueid FROM (SELECT uniqueid, ROW_NUMBER() OVER( PARTITION BY parcelid, 
+						 					propertyaddress,
+						 					saleprice, 
+						 					saledate, 
+						 					legalreference,
+											taxdistrict, 
+											owneraddress, 
+						 					ownername ORDER BY uniqueid ) AS row_num
+											  FROM public.nashville_housing) n 
+ 											    WHERE n.row_num > 1)
+ 
+-- Output
+-- DELETE 103
+
+-- Query returned successfully in 286 msec.
