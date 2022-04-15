cb952a066bd3768f2718fc80066d9d403353c6bd
Nazarudin Ahmad <43535279+nnzrr@users.noreply.github.com>
2022-04-15 22:27:54 +0700

    Create query.sql

---
+++ Delete Duplicates/query.sql
@@ -0,0 +1,29 @@
+--showing duplicate rows
+WITH test AS (SELECT *, ROW_NUMBER() OVER( PARTITION BY parcelid, 
+						 				                                    propertyaddress,
+						 				                                    saleprice, 
+						 			                                    	saledate, 
+						 				                                    legalreference,
+										                                    taxdistrict, 
+										                                    owneraddress, 
+						 				                                    ownername ORDER BY uniqueid ) AS row_num
+										                                      FROM public.nashville_housing)
+ 
+ SELECT * FROM test WHERE row_num > 1
+ 
+ DELETE FROM public.nashville_housing
+WHERE uniqueid IN
+(SELECT uniqueid FROM (SELECT uniqueid, ROW_NUMBER() OVER( PARTITION BY 	parcelid, 
+						 				propertyaddress,
+						 				saleprice, 
+						 				saledate, 
+						 				legalreference,
+										taxdistrict, 
+										owneraddress, 
+						 				ownername ORDER BY uniqueid ) AS row_num
+										FROM public.nashville_housing) n 
+ 										WHERE n.row_num > 1)
+ 
+ DELETE 103
+
+Query returned successfully in 286 msec.
